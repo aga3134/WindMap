b9d61b237c3299f6fcf856096a32bb4802120d6c
aga3134 <aga3134@gmail.com>
2017-09-27 22:14:45 +0800

    add wind particle

--- windMap.js
+++ windMap.js
@@ -1,8 +1,13 @@
 var g_Map;
 var g_Field;
 var g_CurField;
+var g_DrawField = false;
+var g_Particle;
+const MAX_PARTICLE = 1000;
+const MAX_AGE = 50;
+const POS_LEN = 10;
 
-g_FieldColor = d3.scale.linear()
+var g_FieldColor = d3.scale.linear()
 	.domain([0,2,4,6,8,10,12,14,16,18,20,24,27,29])
     .range([d3.rgb(37,74,25), d3.rgb(0,150,254),
       	d3.rgb(18,196,200), d3.rgb(18,211,73),
@@ -12,12 +17,17 @@ g_FieldColor = d3.scale.linear()
       	d3.rgb(202,25,186), d3.rgb(86,54,222),
       	d3.rgb(42,132,222), d3.rgb(64,199,222)]);
 
+var g_ParticleColor = d3.scale.linear()
+		.domain([0,POS_LEN])
+		.range([d3.rgb(255,255,255),d3.rgb(0,0,0)]);
+
 function PadLeft(val, totalLen, ch){
 	var  len = (totalLen - String(val).length)+1;
 	return len > 0? new Array(len).join(ch || '0')+val : val;
 }
 
-function LoadWindData(){
+//==================field======================
+function LoadFieldData(){
 	g_Field = {};
 
 	dataUrl = [	"2017092418",
@@ -31,7 +41,8 @@ function LoadWindData(){
 		var uData = data[0];
 		var vData = data[1];
 		field = {};
-		field.header = uData.header;
+		field.header = Object.assign({}, uData.header);
+
 		if(field.header.la1 > field.header.la2){
 			field.header.la1 = uData.header.la2;
 			field.header.la2 = uData.header.la1;
@@ -41,7 +52,12 @@ function LoadWindData(){
 			field.header.lo2 = uData.header.lo1;
 		}
 
-		field.data = [];
+		var la1 = field.header.la1;
+		var lo1 = field.header.lo1;
+		field.data = {};
+		for(var i=0;i<field.header.nx;i++){
+			field.data[i+lo1] = {};
+		}
 		for(var j=0;j<field.header.ny;j++){
 			for(var i=0;i<field.header.nx;i++){
 				var index = j*field.header.nx+i;
@@ -49,7 +65,7 @@ function LoadWindData(){
 				var v = vData.data[index];	//speed*sinTheta
 				var value = {u: u, v: v, i:i, j:j};
 				value.mag = Math.sqrt(value.u*value.u+value.v*value.v);
-				field.data.push(value);
+				field.data[i+lo1][j+la1] = value;
 			}
 		}
 		g_Field[fieldID] = field;
@@ -57,8 +73,14 @@ function LoadWindData(){
 
 	function LoadDataRec(urlArr, i){
 		if(i >= dataUrl.length){
-			g_CurField = Object.assign({}, g_Field["0"]);
-			return UpdateWindGrid();
+			g_CurField = Object.assign({}, g_Field[0]);
+			DrawWindField();
+			InitParticle();
+			setInterval(function(){
+				UpdateParticle();
+				DrawParticle();
+			}, 30);
+			return;
 		}
 		var url = "/data/"+urlArr[i]+"_UV_SFC.json";
 		$.get(url, function(data){
@@ -74,23 +96,57 @@ function LoadWindData(){
 function InterpolateField(id1, id2, alpha){
 	var field1 = g_Field[id1];
 	var field2 = g_Field[id2];
+	var la1 = field1.header.la1;
+	var lo1 = field1.header.lo1;
 	for(var j=0;j<field1.header.ny;j++){
 		for(var i=0;i<field1.header.nx;i++){
 			var index = j*field1.header.nx+i;
-			d1 = field1.data[index];
-			d2 = field2.data[index];
+			d1 = field1.data[i+lo1][j+la1];
+			d2 = field2.data[i+lo1][j+la1];
 			var value = {u: d1.u*(1-alpha)+d2.u*alpha, v: d1.v*(1-alpha)+d2.v*alpha, i:i, j:j};
 			value.mag = Math.sqrt(value.u*value.u+value.v*value.v);
-			g_CurField.data[index] = value;
+			g_CurField.data[i+lo1][j+la1] = value;
 		}
 	}
 }
 
-function UpdateWindGrid(){
-	if(!g_Map) return;
-	var svg =  d3.select("#windMap");
-	svg.selectAll("*").remove();
+function GetField(lat,lng){
+	if(!g_CurField) return null;
+	var lat1 = parseInt(lat);
+	var lat2 = lat1+1;
+	var lng1 = parseInt(lng);
+	var lng2 = lng1+1;
 
+	if(lat1 < g_CurField.header.la1 || lat1 > g_CurField.header.la2) return null;
+	if(lat2 < g_CurField.header.la1 || lat2 > g_CurField.header.la2) return null;
+	if(lng1 < g_CurField.header.lo1 || lng1 > g_CurField.header.lo2) return null;
+	if(lng2 < g_CurField.header.lo1 || lng2 > g_CurField.header.lo2) return null;
+	var v0 = g_CurField.data[lng1][lat1];
+	var v1 = g_CurField.data[lng2][lat1];
+	var v2 = g_CurField.data[lng2][lat2];
+	var v3 = g_CurField.data[lng1][lat2];
+	var alpha = 1-(lat-lat1);
+	var beta = 1-(lng-lng1);
+	var w0 = {};
+	w0.u = v0.u*alpha+v1.u*(1-alpha);
+	w0.v = v0.v*alpha+v1.v*(1-alpha);
+	var w1 = {};
+	w1.u = v2.u*alpha+v3.u*(1-alpha);
+	w1.v = v2.v*alpha+v3.v*(1-alpha);
+	var v = {};
+	v.u = w0.u*beta+w1.u*(1-beta);
+	v.v = w0.v*beta+w1.v*(1-beta);
+	v.mag = Math.sqrt(v.u*v.u+v.v*v.v);
+	return v;
+}
+
+function DrawWindField(){
+	if(!g_Map) return;
+
+	var svgField = d3.select("#windField");
+	svgField.selectAll("*").remove();
+	if(!g_DrawField) return;
+	
 	var proj = g_Map.getProjection();
 	var bound = g_Map.getBounds();
 	if(!bound) return;
@@ -99,14 +155,17 @@ function UpdateWindGrid(){
 	var tr = proj.fromLatLngToPoint(ne);
 	var bl = proj.fromLatLngToPoint(sw);
 	var scale = Math.pow(2, g_Map.getZoom());
+
 	var windScale = 0.1;
 
 	if(!g_CurField) return;
 
+	var la1 = g_CurField.header.la1;
+	var lo1 = g_CurField.header.lo1;
 	for(var j=0;j<g_CurField.header.ny;j++){
 		for(var i=0;i<g_CurField.header.nx;i++){
 			var index = j*g_CurField.header.nx+i;
-			var wind = g_CurField.data[index];
+			var wind = g_CurField.data[i+lo1][j+la1];
 			var lat = g_CurField.header.la1+g_CurField.header.dy*j;
 			var lng = g_CurField.header.lo1+g_CurField.header.dx*i;
 			var lat2 = lat+wind.v*windScale;
@@ -121,13 +180,128 @@ function UpdateWindGrid(){
     		var x2 = (pt2.x-bl.x)*scale;
     		var y2 = (pt2.y-tr.y)*scale;
 
-    		svg.append("circle").attr("cx",x).attr("cy",y).attr("r",2).attr("fill","red");
-    		svg.append("line").attr("x1",x).attr("x2",x2)
+    		svgField.append("line").attr("x1",x).attr("x2",x2)
     			.attr("y1",y).attr("y2",y2).attr("stroke",g_FieldColor(wind.mag));
 		}
 	}
 }
 
+//======================particle===========================
+function CreateParticle(bound){
+	var latRange = bound.maxLat-bound.minLat;
+	var lngRange = bound.maxLng-bound.minLng;
+	var particle = {};
+	particle.age = Math.random()*MAX_AGE;
+	particle.pos = [];
+	var pos = {lat: Math.random()*latRange+bound.minLat, lng: Math.random()*lngRange+bound.minLng};
+	for(var i=0;i<POS_LEN;i++){
+		particle.pos.push(pos);
+	}
+	return particle;
+}
+
+function GetBound(){
+	if(!g_CurField) return null;
+	var bound = {};
+	bound.minLat = g_CurField.header.la1;
+	bound.maxLat = g_CurField.header.la2;
+	bound.minLng = g_CurField.header.lo1;
+	bound.maxLng = g_CurField.header.lo2;
+	var mapBound = g_Map.getBounds();
+	if(mapBound){
+		ne = mapBound.getNorthEast();
+		sw = mapBound.getSouthWest();
+		if(sw.lat() > bound.minLat) bound.minLat = sw.lat();
+		if(ne.lat() < bound.maxLat) bound.maxLat = ne.lat();
+		if(ne.lng() > bound.minLng) bound.minLng = ne.lng();
+		if(sw.lng() < bound.maxLng) bound.maxLng = sw.lng();
+	}
+	return bound;
+}
+
+function InitParticle(){
+	g_Particle = [];
+	var bound = GetBound();
+	if(!bound) return;
+	
+	for(var i=0;i<MAX_PARTICLE;i++){
+		var particle = CreateParticle(bound);
+		g_Particle.push(particle);
+	}
+}
+
+function UpdateParticle(){
+	var bound = GetBound();
+	if(!bound) return;
+	var scale = Math.pow(2, g_Map.getZoom());
+	var windScale = 0.5/scale;
+
+	for(var i=0;i<MAX_PARTICLE;i++){
+		var p = g_Particle[i];
+		var pos = p.pos[0];
+		var wind = GetField(pos.lat, pos.lng);
+
+		if(!wind){	//ouside field
+			g_Particle[i] = CreateParticle(bound);
+			continue;
+		}
+		for(var j=POS_LEN-1;j>0;j--){
+			p.pos[j] = p.pos[j-1];
+		}
+		var newPos = {lat: p.pos[0].lat+wind.v*windScale, lng: p.pos[0].lng+wind.u*windScale};
+		p.pos[0] = newPos;
+		p.age++;
+		if(p.age >= MAX_AGE){	//delete old particle & create new
+			g_Particle[i] = CreateParticle(bound);
+		}
+	}
+}
+
+function DrawParticle(){
+	if(!g_Map) return;
+
+	var proj = g_Map.getProjection();
+	var bound = g_Map.getBounds();
+	if(!bound) return;
+	ne = bound.getNorthEast();
+	sw = bound.getSouthWest();
+	var tr = proj.fromLatLngToPoint(ne);
+	var bl = proj.fromLatLngToPoint(sw);
+	var scale = Math.pow(2, g_Map.getZoom());
+
+	var svgParticle = d3.select("#windParticle");
+	svgParticle.selectAll("*").remove();
+	for(var i=0;i<MAX_PARTICLE;i++){
+		var p = g_Particle[i];
+
+		var pathArr = [];
+		var skip = false;
+		for(var j=0;j<POS_LEN;j++){
+			var point = new google.maps.LatLng(p.pos[j].lat,p.pos[j].lng);
+			var pt = proj.fromLatLngToPoint(point);
+			if(p.pos[j].lng >= 180){	//超過投影平面邊界
+				skip = true;
+				break;
+			}
+			var x = (pt.x-bl.x)*scale;
+			var y = (pt.y-tr.y)*scale;
+			pathArr.push({x:x, y:y});
+		}
+		if(skip) continue;
+		var curve = d3.svg.line()
+                   .x(function(d) { return d.x; })
+                   .y(function(d) { return d.y; })
+                   .interpolate("bundle");
+
+		svgParticle.append("path")
+			.attr("d",curve(pathArr))
+			.attr("fill","None")
+			.attr("stroke","url(#whiteGradient)");
+			
+	}
+}
+
+//=========================================================
 function InitMap() {
 	var taiwan = new google.maps.LatLng(23.682094,120.7764642);
 
@@ -138,21 +312,32 @@ function InitMap() {
 		mapTypeId: 'hybrid'
 	});
 
-	google.maps.event.addListener(g_Map, 'bounds_changed', function() {
-		var svg =  d3.select("#windMap");
-		svg.selectAll("*").remove();
-		//UpdateWindGrid();
+	google.maps.event.addListener(g_Map, 'idle', function() {
+		//var windField =  d3.select("#windField");
+		//windField.selectAll("*").remove();
+		DrawWindField();
     });
 
+    g_Map.addListener('dragstart', function() {
+		var windField =  d3.select("#windField");
+		windField.selectAll("*").remove();
+	});
+
     g_Map.addListener('dragend', function() {
-		UpdateWindGrid();
+		//DrawWindField();
 	});
 
 	g_Map.addListener('zoom_changed', function() {
-		UpdateWindGrid();
+		if (g_Map.getZoom() < 4) g_Map.setZoom(4);
+		//DrawWindField();
+	});
+
+	$("#toggleField").change(function(){
+		g_DrawField = $("#toggleField").prop("checked");
+		DrawWindField();
 	});
 	
-	LoadWindData();
+	LoadFieldData();
 }
 
 google.maps.event.addDomListener(window, 'load', InitMap);
